2a315e377a72b25503b4ffa36f10551320ba1903
Ben Hoberg <benjhoberg@gmail.com>
2023-09-26 12:36:45 -0500

    Loading animation and state management optimized for now.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,12 @@ import { type NextPage } from "next";
 import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativetime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+import { LoadingAnimation } from "~/components/loadingAnimation";
 
 dayjs.extend(relativetime);
 
+// CREATES POSTS
 const CreatePostWizard = () => {
   const { user } = useUser();
 
@@ -15,10 +18,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex gap-3">
-      <img
+      <Image
         src={user.imageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -28,13 +33,20 @@ const CreatePostWizard = () => {
   );
 };
 
+// INITIALIZES NEW TYPE 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
+// EACH POST WILL BE CREATED WITH THIS FUNCION IN A .MAP()
 const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profilePicture} className="h-14 w-14 rounded-full" />
+      <Image src={author.profilePicture} 
+        className="h-14 w-14 rounded-full" 
+        alt="{`@{author.username}'s profile picture"
+        height={56}
+        width={56}
+        />
       <div className="flex flex-col">
         <div className="flex gap-1 font-bold text-slate-300">
           <span>{`@${author.username!}`}</span>
@@ -46,12 +58,30 @@ const PostView = (props: PostWithUser) => {
   );
 };
 
+// LOOP THROUGH ALL POSTS USING POSTVIEW FUNCTION ABOVE
+const Feed = () => {
+  const { data, isLoading: postsLoaded } = api.posts.getAll.useQuery();
+
+if (postsLoaded) return <LoadingAnimation />;
+
+  if (!data) return <div>Something went wrong</div>;
+
+  return (
+    <div className="flex flex-col">
+            {data?.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
+            ))}
+          </div>
+  )
+}
+
+// NOW PUT IT ALL TOGETHER AND PARTY 
 const Home: NextPage = () => {
-  const user = useUser();
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  const { data, isLoading } = api.posts.getAll.useQuery();
+  api.posts.getAll.useQuery();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (!userLoaded) return  <div />;
 
   //if (!data) return (<div>Something went wrong</div>);
 
@@ -60,22 +90,18 @@ const Home: NextPage = () => {
       <main className="flex h-screen justify-center">
         <div className="md:mad-w-2xl h-full w-full  border-x border-slate-400">
           <div className="flex border-b border-slate-400 p-4">
-            {!user.isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">
                 <SignInButton />
               </div>
             )}
-            {!!user.isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">
                 <CreatePostWizard />
               </div>
             )}
           </div>
-          <div className="flex flex-col">
-            {data?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
